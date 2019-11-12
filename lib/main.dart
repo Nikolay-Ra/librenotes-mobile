4ce0bacb3565e11ff7f5417235dd310a90b0be42
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-12 19:46:06 +0300

    add themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:librenotes/routes.dart';
+import 'package:librenotes/styles/themes.dart';
 
 void main() => runApp(App());
 
@@ -8,6 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LibreNotes',
+      theme: dark,
       initialRoute: initialRoute,
       routes: routes,
     );
---
+++ lib/styles/themes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final light = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.blue,
+  accentColor: Colors.orange[400],
+);
+
+final dark = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.blue[900],
+  accentColor: Colors.orange[600],
+);
