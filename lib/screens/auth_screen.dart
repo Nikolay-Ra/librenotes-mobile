b4a29c098a075b9514c4658d6fd746881f237e82
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-13 22:13:39 +0300

    add auth screen layout

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -6,16 +6,97 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text('Auth'),
-          onPressed: () {
-            Navigator.of(context).pushNamed('notes');
-          },
-        )
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _getLogo(),
+            _getForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getLogo() {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2,
+      padding: EdgeInsets.only(top: 16),
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Icon(
+          Icons.event_note,
+          color: Theme.of(context).accentColor,
+        ),
+      ),
+    );
+  }
+
+  _getForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Server address',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Server address can not be empty';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'User name',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'User name can not be empty';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Password can not be empty';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Navigator.pushReplacementNamed(context, 'notes');
+                      }
+                    },
+                    child: Text('Log In'),
+                    color: Theme.of(context).brightness == Brightness.dark ? Colors.blue[900] : Theme.of(context).buttonColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
