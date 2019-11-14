d2e828b40ffb45945db6ac32c5db15fd2c9454d4
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-14 14:53:38 +0300

    add notes screen layout

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:librenotes/models/note.dart';
+import 'package:librenotes/widgets/note_card.dart';
 
 class NotesScreen extends StatefulWidget {
   @override
@@ -6,20 +8,59 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+  List<Note> notes = [ // TODO: temporary models for layout design
+    Note(id: 1, text: 'Update project roadmap'),
+    Note(id: 2, text: 'Buy cookies 🍪🍪🍪'),
+    Note(id: 3, text: 'Read Flutter docs!\nhttps://flutter.dev/docs'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Notes'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Edit'),
-          onPressed: () {
-            Navigator.of(context).pushNamed('notes/edit');
-          },
-        ),
+      body: _getBody(),
+    );
+  }
+
+  _getBody() {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        var pos = notes.length - 1 - index;
+        return _buildNoteItem(context, pos);
+      },
+    );
+  }
+
+  _buildNoteItem(context, int pos) {
+    var note = notes[pos];
+
+    return Dismissible(
+      key: Key(note.id.toString()),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 8, top: 8, right: 8),
+              child: NoteCard(
+                note: note,
+                onTap: () {
+                  Navigator.pushNamed(context, 'notes/edit', arguments: note);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
+      onDismissed: (direction) {
+        setState(() {
+          notes.removeAt(pos);
+        });
+
+        Scaffold.of(context).showSnackBar(SnackBar(content: Text('Note removed')));
+      },
     );
   }
 }
