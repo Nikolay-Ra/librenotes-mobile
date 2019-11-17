37a3b87f6b15a95310d5d5514d9bfee0b659760c
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-17 15:09:48 +0300

    add tag selector to edit note screen

--- lib/screens/edit_note_screen.dart
+++ lib/screens/edit_note_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:librenotes/models/note.dart';
+import 'package:librenotes/widgets/tag_button.dart';
+import 'package:librenotes/widgets/toggle_tag.dart';
 
 class EditNoteScreen extends StatefulWidget {
   @override
@@ -12,9 +14,14 @@ class _NotesScreenState extends State<EditNoteScreen> {
 
   final noteTextController = TextEditingController();
 
+  final tags = ['TODO', 'Work', 'Study'];
+  List<bool> tagsState;
+
   @override
   void initState() {
     super.initState();
+
+    tagsState = List.filled(tags.length, false);
 
     SchedulerBinding.instance.addPostFrameCallback((_) {
       note = ModalRoute.of(context).settings.arguments;
@@ -36,22 +43,58 @@ class _NotesScreenState extends State<EditNoteScreen> {
       ),
       body: Column(
         children: <Widget>[
-          Expanded(
-            child: TextField(
-              autofocus: true,
-              expands: true,
-              maxLines: null,
-              controller: noteTextController,
-              decoration: new InputDecoration(
-                contentPadding: EdgeInsets.all(8),
-                border: InputBorder.none,
-                hintText: 'Write your note here',
-              ),
+          _getTagSelector(),
+          _getTextEditor(),
+        ],
+      ),
+      floatingActionButton: _getFloatingActionButton(),
+    );
+  }
+
+  _getTagSelector() {
+    return Container(
+      padding: EdgeInsets.all(8),
+      color: Theme.of(context).cardColor,
+      child: Row(
+        children: <Widget>[
+          for (var i = 0; i < tags.length; i++)
+          Padding(
+            padding: EdgeInsets.only(right: 8),
+            child: ToggleTag(
+              name: tags[i],
+              active: tagsState[i],
+              onTap: () {
+                setState(() {
+                  tagsState[i] = !tagsState[i];
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 8),
+            child: TagButton(
+              name: ' + ',
+              onTap: () {},
             ),
           ),
         ],
       ),
-      floatingActionButton: _getFloatingActionButton(),
+    );
+  }
+
+  _getTextEditor() {
+    return Expanded(
+      child: TextField(
+        autofocus: true,
+        expands: true,
+        maxLines: null,
+        controller: noteTextController,
+        decoration: new InputDecoration(
+          contentPadding: EdgeInsets.all(8),
+          border: InputBorder.none,
+          hintText: 'Write your note here',
+        ),
+      ),
     );
   }
 
