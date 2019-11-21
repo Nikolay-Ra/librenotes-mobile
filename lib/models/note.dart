37e48e43e1f7e9a839f683612676c5038fd2095a
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-21 16:18:24 +0300

    update note model

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,8 +1,13 @@
+import 'package:librenotes/models/tag.dart';
 import 'package:meta/meta.dart';
 
 class Note {
   final int id;
+  final DateTime created;
+  final List<Tag> tags;
   final String text;
 
-  Note({@required this.id, @required this.text});
+  Note({@required this.id, DateTime created, List<Tag> tags, this.text: ''}) :
+    this.created = created ?? DateTime.now(),
+    this.tags = List.unmodifiable(tags ?? []);
 }
