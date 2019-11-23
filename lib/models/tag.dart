196f3a0b95093ea2e75753466badb048d0fa8e3e
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-23 14:38:59 +0300

    add serialize methods to Tag model

--- lib/models/tag.dart
+++ lib/models/tag.dart
@@ -1,8 +1,21 @@
-import 'package:meta/meta.dart';
-
 class Tag {
   final int id;
   final String name;
 
-  Tag({@required this.id, @required this.name});
+  Tag({this.id, this.name: ''});
+
+  factory Tag.fromMap(Map<String, dynamic> map) => Tag(
+    id: map['id'],
+    name: map['name'],
+  );
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'name': name,
+  };
+
+  @override
+  String toString() {
+    return 'Tag{id: $id, name: $name}';
+  }
 }
