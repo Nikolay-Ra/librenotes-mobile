538d454b404f9b7aff474a3617808c6fc7c83192
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-24 21:49:30 +0300

    use cache service in storage provider

--- lib/providers/storage.dart
+++ lib/providers/storage.dart
@@ -3,8 +3,11 @@ import 'dart:collection';
 import 'package:flutter/widgets.dart';
 import 'package:librenotes/models/note.dart';
 import 'package:librenotes/models/tag.dart';
+import 'package:librenotes/services/cache.dart';
 
 class Storage with ChangeNotifier {
+  Cache cache = Cache();
+
   List<Note> _notes = [];
   List<Tag> _tags = [];
 
@@ -12,37 +15,92 @@ class Storage with ChangeNotifier {
   get tags => UnmodifiableListView(_tags);
 
   Storage() {
-    _tags.addAll([
-      Tag(id: 1, name: 'TODO'),
-      Tag(id: 2, name: 'Work'),
-      Tag(id: 3, name: 'Study'),
-    ]);
-
-    _notes.addAll([
-      Note(id: 1, text: 'Update project roadmap', tags: [_tags[0], _tags[1], _tags[2]]),
-      Note(id: 2, text: 'Buy cookies 🍪🍪🍪'),
-      Note(id: 3, text: 'Read Flutter docs!\nhttps://flutter.dev/docs', tags: [_tags[1]]),
-    ]);
+    _load();
   }
 
-  void addNote(Note note) {
-    _notes.add(note);
+  _load() async {
+    cache.firstOpen.then(
+      (first) async {
+        if (first) {
+          await cache.insertDebugData();
+        }
+
+        _tags = await cache.tags();
+        _notes = await cache.notes();
+        notifyListeners();
+      }
+    );
+  }
+
+  void addNote(Note note) async {
+    int id = await cache.insertNote(note);
+
+    _notes.add(Note(
+      id: id,
+      created: note.created,
+      tags: note.tags,
+      text: note.text,
+    ));
     notifyListeners();
   }
 
   void saveNote(Note note) {
     int index = _notes.indexWhere((second) => second.id == note.id);
+
     _notes[index] = note;
     notifyListeners();
+
+    cache.updateNote(note);
   }
 
   void deleteNote(int id) {
     _notes.removeWhere((note) => note.id == id);
     notifyListeners();
+
+    cache.deleteNote(id);
   }
 
-  void addTag(Tag tag) {
-    _tags.add(tag);
+  Tag getTag(int id) {
+    return _tags.firstWhere(
+      (tag) => tag.id == id,
+      orElse: () => null,
+    );
+  }
+
+  void addTag(Tag tag) async {
+    int id = await cache.insertTag(tag);
+
+    _tags.add(Tag(
+      id: id,
+      name: tag.name,
+    ));
     notifyListeners();
+  }
+
+  void saveTag(Tag tag) {
+    int index = _notes.indexWhere((second) => second.id == tag.id);
+
+    _tags[index] = tag;
+    notifyListeners();
+
+    cache.updateTag(tag);
+  }
+
+  void deleteTag(int id) {
+    for (var i = 0; i < _notes.length; i++) {
+      Note note = _notes[i];
+      if (note.tags.contains(id)) {
+        _notes[i] = Note(
+          id: id,
+          created: note.created,
+          tags: note.tags.where((tag) => tag != id),
+          text: note.text,
+        );
+      }
+    }
+    _tags.removeWhere((tag) => tag.id == id);
+    notifyListeners();
+
+    cache.deleteTag(id);
   }
 }
